5739de85a19660b6d2a3772ccfac9ddc2b0cfffd
goran <email>
2021-10-10 20:13:18 +0200

    hetzner cloud server

---
+++ hetzner/terraform/servers.tf
@@ -0,0 +1,15 @@
+resource "hcloud_server" "nextcloud" {
+  name        = "fin-cx21c-nextcloud"
+  image       = "debian-11"
+  server_type = "cx21-ceph"
+  location    = "hel1"
+  ssh_keys    = [hcloud_ssh_key.default.id]
+  keep_disk   = true
+  backups     = false
+}
+
+resource "hcloud_server_network" "nextcloud-network" {
+  server_id  = hcloud_server.nextcloud.id
+  network_id = hcloud_network.lab.id
+  ip         = "10.0.1.10"
+}
