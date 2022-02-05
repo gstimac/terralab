d91f656026420a59b0c25fc3bf83c6f63e8446b8
Goran Stimac <g.stimac@email.com>
2022-02-05 18:57:20 +0100

    feat: ADO keda job autoscaling

--- azure/aks/acr.tf
+++ azure/aks/acr.tf
@@ -1,10 +1,12 @@
 data "azurerm_container_registry" "acr" {
+  count               = var.acr_name == "" ? 0 : 1
   name                = "${var.environment}eastusacr"
   resource_group_name = "main-${var.location.short}-acr-RG"
 }
 
 resource "azurerm_role_assignment" "acr" {
-  scope                            = data.azurerm_container_registry.acr.id
+  count                            = var.acr_name == "" ? 0 : 1
+  scope                            = data.azurerm_container_registry.acr[0].id
   role_definition_name             = "acrpull"
   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   skip_service_principal_aad_check = true
--- azure/aks/main.tf
+++ azure/aks/main.tf
@@ -6,36 +6,40 @@ locals {
 }
 
 resource "azurerm_resource_group" "aks" {
-  name     = "main-${var.location.short}-aks-RG"
+  name     = "RG-${var.location.short}-aks-${var.cluster_name}"
   location = var.location.full
   tags     = local.tags
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                    = var.cluster_name
-  location                = azurerm_resource_group.aks.location
-  resource_group_name     = azurerm_resource_group.aks.name
-  dns_prefix              = var.dns_prefix
-  kubernetes_version      = "1.20.2"
+  name                = var.cluster_name
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  dns_prefix          = var.dns_prefix
+  #kubernetes_version      = "1.20.2"
   private_cluster_enabled = false
   tags                    = local.tags
 
   default_node_pool {
-    os_disk_size_gb     = "30"
-    os_disk_type        = "Managed"
-    name                = "default"
+    name                = "system"
     node_count          = var.node_count
     vm_size             = var.default_node_pool_vm_size
+    os_disk_size_gb     = "30"
     vnet_subnet_id      = data.azurerm_subnet.sn.id
-    enable_auto_scaling = false
-    //    min_count            = var.auto_scaling_min
-    //    max_count            = var.auto_scaling_max
-    tags = local.tags
+    enable_auto_scaling = true
+    min_count           = var.auto_scaling_min
+    max_count           = var.auto_scaling_max
+    tags                = local.tags
+    type                = "VirtualMachineScaleSets"
+
+    node_labels = {
+      "application" = "utils"
+    }
   }
 
   network_profile {
     network_plugin    = "kubenet"
-    load_balancer_sku = "basic"
+    load_balancer_sku = "Standard"
     outbound_type     = "loadBalancer"
   }
 
@@ -49,9 +53,32 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   linux_profile {
     admin_username = "ubuntu"
-
     ssh_key {
       key_data = var.ssh_public_key
     }
   }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "nodepool" {
+  name                  = "adospot"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  vm_size               = "Standard_D2a_v4"
+  node_count            = 0
+  min_count             = 0
+  max_count             = 3
+  priority              = "Spot"
+  enable_auto_scaling   = var.enable_autoscaling
+  eviction_policy       = "Delete"
+  spot_max_price        = -1
+  os_disk_size_gb       = "30"
+
+  node_labels = {
+    "kubernetes.azure.com/scalesetpriority" = "spot"
+    "application"                           = "ADO"
+  }
+  node_taints = [
+    "kubernetes.azure.com/scalesetpriority=spot:NoSchedule"
+  ]
+
+  tags = local.tags
 }
